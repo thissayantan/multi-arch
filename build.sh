e8b0d6af756a7fdaabef7ffda6a57998bc18bd41
Sayantan <this.sayantan@outlook.com>
2022-12-15 19:24:57 +0700

    feat: npm i works

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_BUILDKIT=0
+# DOCKER_BUILDKIT=0
 COMPOSE_DOCKER_CLI_BUILD=0
-echo $DOCKER_BUILDKIT
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --tag thissaynatan/multi-arch:latest --push .
+docker run -it --rm --privileged tonistiigi/binfmt --install all
+docker buildx build --platform linux/amd64,linux/arm64 --tag thissayantan/multi-arch:latest --push .
